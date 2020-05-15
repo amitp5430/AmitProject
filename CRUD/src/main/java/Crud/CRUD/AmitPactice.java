c705c597cea2a1d075cd465c5a7961fb004f58bf
Amit's <Amit's@Amits-PC>
2020-05-15 12:41:20 +0530

    New Class Added

---
+++ CRUD/src/main/java/Crud/CRUD/AmitPactice.java
@@ -0,0 +1,26 @@
+package Crud.CRUD;
+
+public class AmitPactice {
+	private int id;
+	private String name;
+	private String Catogory;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCatogory() {
+		return Catogory;
+	}
+	public void setCatogory(String catogory) {
+		Catogory = catogory;
+	}
+
+}
